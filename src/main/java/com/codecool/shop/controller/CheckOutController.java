1d1f64fbcf1badd78ed914800194052fcfcffae0
samrcell91 <smarcell91@gmail.com>
2021-06-02 16:57:12 +0200

    Checkout controller base

--- src/main/java/com/codecool/shop/controller/CheckOutController.java
+++ src/main/java/com/codecool/shop/controller/CheckOutController.java
@@ -1,7 +1,8 @@
 package com.codecool.shop.controller;
 
-
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -13,13 +14,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/checkout"})
-public class ChechkOutController extends HttpServlet {
+public class CheckOutController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        CartDao cartDataStore = CartDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        context.setVariable("cart", cartDataStore.getCart());
+
+        engine.process("product/checkout.html", context, resp.getWriter());
     }
 }
