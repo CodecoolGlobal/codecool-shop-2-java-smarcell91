81c529616d2e804103d91065ddc284c7ef6bf016
samrcell91 <smarcell91@gmail.com>
2021-06-15 20:40:43 +0200

    in progress

--- src/main/java/com/codecool/shop/dao/database/CartDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/database/CartDaoJdbc.java
@@ -2,9 +2,11 @@ package com.codecool.shop.dao.database;
 
 import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.User;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,54 +18,90 @@ public class CartDaoJdbc implements CartDao {
         this.dataSource = dataSource;
     }
 
-//    @Override
-//    public void add(Product product, User user) {
-//        try (Connection conn = dataSource.getConnection()) {
-//            String sql = "INSERT INTO cart (user_id, product_id) VALUES (?, ?)";
-//            PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-//            st.setInt(1, user.getId());
-//            st.setInt(2, product.getId());
-//            st.executeUpdate();
-//            ResultSet rs = st.getGeneratedKeys();
-//            rs.next(); // Read next returned value - in this case the first one. See ResultSet docs for more explaination
-//            state.setId(rs.getInt(1));
-//
-//        } catch (SQLException throwables) {
-//            throw new RuntimeException("Error while adding new game state", throwables);
-//        }
-//    }
+    @Override
+    public void add(Product product, int userId) {
+        try (Connection conn = dataSource.getConnection()) {
+            String table = "create table IF NOT EXISTS cart"+userId+" (" + "product_id INT," + ")";
 
-    @Override
-    public Product find(int id) {
-        return null;
-    }
-    @Override
-    public void remove(int id) {
-        //remove
+            String sql = "INSERT INTO ? (product_id) VALUES (?)";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.execute(table);
+            st.setString(1, "cart"+userId);
+            st.setInt(2, product.getId());
+            st.executeUpdate();
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Error while adding "+product.getId()+" product to cart"+userId+" cart.", throwables);
+        }
     }
 
     @Override
-    public void decrementAmount(Product product) {
-        //minus
+    public Product find(int productId, int userId) {
+        try (Connection c = dataSource.getConnection()) {
+            String sql = "SELECT product_id FROM ? ";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, "cart"+userId);
+            ResultSet rs = ps.executeQuery();
+            List<Product> result = new ArrayList<>();
+            while (rs.next()) { // while result set pointer is positioned before or on last row read authors
+                // get product by id and create a product object
+                Product product = new Product()  //(rs.getString(2), rs.getInt(3), rs.getInt(4), rs.getInt(5), rs.getString(6), rs.getInt(7));
+                product.setId(rs.getInt(1));
+                result.add(product);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all players", e);
+        }
     }
 
     @Override
-    public Map<Product, Integer> getCart() {
-        return null;
+    public void remove(int productId, int userId) {
+        try (Connection c = dataSource.getConnection()){
+            String sql = "DELETE FROM ? WHERE product_id = ?";
+            PreparedStatement st = c.prepareStatement(sql);
+            st.setString(1, "cart"+userId);
+            st.setInt(2, productId);
+            st.executeUpdate();
+        } catch(SQLException e) {
+            System.out.println(e);
+        }
     }
 
     @Override
-    public void setCart(Map<Product, Integer> cart) {
-
+    public void decrementAmount(int productId, int userId) {
+        try (Connection c = dataSource.getConnection()){
+            String sql = "DELETE FROM ? WHERE product_id = ? LIMIT 1";
+            PreparedStatement st = c.prepareStatement(sql);
+            st.setString(1, "cart"+userId);
+            st.setInt(2, productId);
+            st.executeUpdate();
+        } catch(SQLException e) {
+            System.out.println(e);
+        }
     }
 
     @Override
-    public List<String> getProductsNames() {
-        return null;
+    public List<Product> getCart(int userId) {
+        try (Connection c = dataSource.getConnection()) {
+            String sql = "SELECT product_id FROM ? ";
+            PreparedStatement ps = c.prepareStatement(sql);
+            ps.setString(1, "cart"+userId);
+            ResultSet rs = ps.executeQuery();
+            List<Product> result = new ArrayList<>();
+            while (rs.next()) { // while result set pointer is positioned before or on last row read authors
+                // get product by id and create a product object
+                Product product = new Product()  //(rs.getString(2), rs.getInt(3), rs.getInt(4), rs.getInt(5), rs.getString(6), rs.getInt(7));
+                product.setId(rs.getInt(1));
+                result.add(product);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all players", e);
+        }
     }
 
     @Override
-    public int getCartSize() {
-        return 0;
+    public void setCart(Map<Integer, List<Product>> cart) {
+
     }
 }
