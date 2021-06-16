32094ccd45878951bc9333b511d98d8c73caa33e
Dániel Fehér <feherdaniel2010@gmail.com>
2021-06-16 14:21:02 +0200

    removed getAll method

--- src/main/java/com/codecool/shop/dao/BillingDao.java
+++ src/main/java/com/codecool/shop/dao/BillingDao.java
@@ -1,12 +1,9 @@
 package com.codecool.shop.dao;
 
-import java.util.List;
-
 import com.codecool.shop.model.Billing;
 
 public interface BillingDao {
     void add(Billing billing, int userId);
     Billing find(int userId);
     void remove(int id);
-    List<Billing> getAll();
 }
