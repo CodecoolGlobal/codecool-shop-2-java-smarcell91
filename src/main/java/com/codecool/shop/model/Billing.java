b6919524c5976e439408b030e0a6a48a30043c44
Dániel Fehér <feherdaniel2010@gmail.com>
2021-06-16 14:21:45 +0200

    added userId field

--- src/main/java/com/codecool/shop/model/Billing.java
+++ src/main/java/com/codecool/shop/model/Billing.java
@@ -2,16 +2,18 @@ package com.codecool.shop.model;
 
 public class Billing {
     private int id;
+    private int userId;
     private String country;
     private String city;
     private int zipcode;
     private String address;
 
-    public Billing(String country, String city, int zipcode, String address) {
+    public Billing(String country, String city, int zipcode, String address, int userId) {
         this.country = country;
         this.city = city;
         this.zipcode = zipcode;
         this.address = address;
+        this.userId = userId;
     }
 
     public String getCountry() {
@@ -36,5 +38,9 @@ public class Billing {
     
     public int getId() {
         return id;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 }
--- src/main/java/com/codecool/shop/model/Shipping.java
+++ src/main/java/com/codecool/shop/model/Shipping.java
@@ -1,17 +1,19 @@
 package com.codecool.shop.model;
 
 public class Shipping {
+    private int userId;
     private int id;
     private String country;
     private String city;
     private int zipcode;
     private String address;
 
-    public Shipping(String country, String city, int zipcode, String address) {
+    public Shipping(String country, String city, int zipcode, String address, int userId) {
         this.country = country;
         this.city = city;
         this.zipcode = zipcode;
         this.address = address;
+        this.userId = userId;
     }
 
     public String getCountry() {
@@ -36,5 +38,9 @@ public class Shipping {
     
     public int getId() {
         return id;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 }
